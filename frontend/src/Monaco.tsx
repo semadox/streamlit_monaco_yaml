a6c9c6f714efb9622ed8569ea3af9cd8008bd616
Julian David Rath <julian.rath@semadox.com>
2023-09-07 17:45:23 +0200

    updated all components

--- frontend/src/Monaco.tsx
+++ frontend/src/Monaco.tsx
@@ -3,8 +3,13 @@ import * as monaco from "monaco-editor";
 // @ts-expect-error No TS module
 import { CancellationToken } from "monaco-editor/esm/vs/base/common/cancellation";
 // @ts-expect-error No TS module
-import { getDocumentSymbols } from "monaco-editor/esm/vs/editor/contrib/documentSymbols/documentSymbols";
-import { setDiagnosticsOptions } from "monaco-yaml";
+// import { getDocumentSymbols } from "monaco-editor/esm/vs/editor/contrib/documentSymbols/documentSymbols";
+// import { setDiagnosticsOptions } from "monaco-yaml";
+
+import { ILanguageFeaturesService } from 'monaco-editor/esm/vs/editor/common/services/languageFeatures.js';
+import { OutlineModel } from 'monaco-editor/esm/vs/editor/contrib/documentSymbols/browser/outlineModel.js';
+import { StandaloneServices } from 'monaco-editor/esm/vs/editor/standalone/browser/standaloneServices.js';
+import { configureMonacoYaml, type SchemasSettings } from 'monaco-yaml'
 // @ts-expect-error No TS module
 import throttle from "raf-throttle";
 import React, { useEffect, useRef } from "react";
@@ -22,23 +27,26 @@ interface AceProps extends ComponentProps {
 
 const modelUri = monaco.Uri.parse("schemas://zerek");
 
+
+async function getDocumentSymbols(ed: any) {
+  const { documentSymbolProvider } = StandaloneServices.get(ILanguageFeaturesService);
+  const outline = await OutlineModel.create(documentSymbolProvider, ed.getModel());
+  return outline.asListOfDocumentSymbols();
+}
+
 // Based on https://github.com/remcohaszing/monaco-yaml/blob/979ed62d6fa1f8a381251bb50aa003190bdd1d19/examples/demo/src/index.ts#L28
 (window as any).MonacoEnvironment = {
-  getWorker(_moduleId: string, label: string) {
+  getWorker(moduleId: string, label: string) {
     switch (label) {
-      case "editorWorkerService":
-        return new Worker(
-          new URL("monaco-editor/esm/vs/editor/editor.worker", import.meta.url),
-        );
-      case "yaml":
-        return new Worker(
-          new URL("monaco-yaml/lib/esm/yaml.worker", import.meta.url),
-        );
+      case 'editorWorkerService':
+        return new Worker(new URL('monaco-editor/esm/vs/editor/editor.worker', import.meta.url))
+      case 'yaml':
+        return new Worker(new URL('monaco-yaml/yaml.worker', import.meta.url))
       default:
-        throw new Error(`Unknown label ${label}`);
+        throw new Error(`Unknown label ${label}`)
     }
-  },
-};
+  }
+}
 
 // Based on https://github.com/remcohaszing/monaco-yaml/blob/979ed62d6fa1f8a381251bb50aa003190bdd1d19/examples/demo/src/index.ts#L158
 function* iterateSymbols(
@@ -64,22 +72,17 @@ function Monaco({ args }: AceProps) {
       throw new Error("Container is not available");
     }
 
-    setDiagnosticsOptions({
+    const defaultSchema: SchemasSettings = {
+      uri: 'https://github.com/remcohaszing/monaco-yaml/blob/HEAD/examples/demo/src/schema.json',
+      // @ts-expect-error TypeScript can’t narrow down the type of JSON imports
+      schema: currentArgs.current.schema,
+      fileMatch: ['monaco-yaml.yaml']
+    }
+
+    const monacoYaml = configureMonacoYaml(monaco, {
       enableSchemaRequest: true,
-      hover: true,
-      completion: true,
-      validate: true,
-      format: true,
-      schemas: [
-        {
-          // Id of the first schema
-          uri: String(modelUri),
-          // Associate with our model
-          fileMatch: [String(modelUri)],
-          schema: currentArgs.current.schema,
-        },
-      ],
-    });
+      schemas: [defaultSchema]
+    })
 
     const model = monaco.editor.createModel(
       currentArgs.current.value,
@@ -104,9 +107,7 @@ function Monaco({ args }: AceProps) {
       position: monaco.Position,
     ) {
       const symbols = await getDocumentSymbols(
-        editor.getModel(),
-        false,
-        CancellationToken.None,
+        editor,
       );
       selectionPath = Array.from(iterateSymbols(symbols, position));
       updateValue();
